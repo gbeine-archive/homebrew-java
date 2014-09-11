bce66fbc6d8649010ca37bbef4ff3f306a802bb4
Gerrit Beine <gerrit.beine@gmx.de>
2014-09-11 20:45:30 +0200

    Update PostgreSQL JDBC to 9.3-1102

--- postgresql-jdbc.rb
+++ postgresql-jdbc.rb
@@ -1,19 +1,19 @@
 require 'formula'
 
 class PostgresqlJdbc < Formula
-  homepage 'http://jdbc.postgresql.org/about/about.html'
-  url 'http://jdbc.postgresql.org/download/postgresql-9.3-1100.jdbc3.jar'
-  sha1 '682c1724724a2fbbd338ba9336686696558e2a5f'
-  version '9.3-1100'
+  homepage "http://jdbc.postgresql.org/about/about.html"
+  url "http://jdbc.postgresql.org/download/postgresql-9.3-1102.jdbc3.jar"
+  sha1 "bad73013364e340cd8dcaea92ec010fc022547f1"
+  version "9.3-1102"
 
-  resource 'jdbc4' do
-    url 'http://jdbc.postgresql.org/download/postgresql-9.3-1100.jdbc4.jar'
-    sha1 'fe6ed26ff71d75ebb99e2ec2123003e05ea16dc5'
+  resource "jdbc4" do
+    url "http://jdbc.postgresql.org/download/postgresql-9.3-1102.jdbc4.jar"
+    sha1 "b1f7eea7af1ee9afdd5a43e8935154e9e206fe01"
   end
 
-  resource 'jdbc41' do
-    url 'http://jdbc.postgresql.org/download/postgresql-9.3-1100.jdbc41.jar'
-    sha1 '8f89bd15ef7b9a7db03b13a0d69ace8581916160'
+  resource "jdbc41" do
+    url "http://jdbc.postgresql.org/download/postgresql-9.3-1102.jdbc41.jar"
+    sha1 "de013bb55a400e83f18523d2b944fac199183796"
   end
 
   option "with-jdbc4", "Install JDBC 4 support"
@@ -21,15 +21,15 @@ class PostgresqlJdbc < Formula
 
   def install
     if build.include? "with-jdbc4"
-      resource('jdbc4').stage do
-        libexec.install Dir['*.jar']
+      resource("jdbc4").stage do
+        libexec.install Dir["*.jar"]
       end
     end
     if build.include? "with-jdbc41"
-      resource('jdbc41').stage do
-        libexec.install Dir['*.jar']
+      resource("jdbc41").stage do
+        libexec.install Dir["*.jar"]
       end
     end
-    libexec.install Dir['*.jar']
+    libexec.install Dir["*.jar"]
   end
 end
