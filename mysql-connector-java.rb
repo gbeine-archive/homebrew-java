0e6b224883393bf8ab3a702480205c8f116c1dfc
Soc Sieng <socsieng@gmail.com>
2015-11-15 21:25:48 +1100

    Update MySQL Connector/J to 5.1.37

--- mysql-connector-java.rb
+++ mysql-connector-java.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class MysqlConnectorJava < Formula
   homepage "http://dev.mysql.com/downloads/connector/j/"
-  url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.32.tar.gz"
-  sha1 "6fe65d48883c0eec50041290999dfa4fea3564a9"
+  url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.37.tar.gz"
+  version "5.1.37"
+  sha256 "01e2f104c169863b4937a77045b008a372e5e254b803f69f030986167e626824"
 
   def install
     prefix.install_metafiles
