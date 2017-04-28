2d7cda1f23835f8b08ff177c6ad0261ddcdb1993
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2017-04-28 14:26:40 -0400

    Update derby to v10.13.1.1

--- derby.rb
+++ derby.rb
@@ -2,20 +2,21 @@ require "formula"
 
 class Derby < Formula
   homepage "http://db.apache.org/derby/"
-  url "http://mirror.serversupportforum.de/apache/db/derby/db-derby-10.11.1.1/db-derby-10.11.1.1-bin.tar.gz"
-  sha256 "68e06b5e859282b6bd7cfc1a801d1bf855a589515e14b5f3e8ea732d76c3fd34"
+  version="10.13.1.1"
+  url "http://mirror.serversupportforum.de/apache/db/derby/db-derby-#{version}/db-derby-#{version}-bin.tar.gz"
+  sha256 "74c35d7ec859b8438e69fd0d1b415dd4f4572f71d2f35fc612f2f21848eef3b3"
 
   option "with-examples", "Install examples"
   option "with-docs", "Install documentation"
 
   def install
     # Remove Windows scripts
-    rm_rf Dir['bin/*.bat']
+    rm_rf Dir["bin/*.bat"]
 
     prefix.install_metafiles
-    libexec.install Dir['bin', 'lib']
-    (libexec/'examples').install Dir['demo/*'] if build.with? 'examples'
-    doc.install Dir['docs/*', 'javadoc'] if build.with? 'docs'
+    libexec.install Dir["bin", "lib"]
+    (libexec/"examples").install Dir["demo/*"] if build.with? "examples"
+    doc.install Dir["docs/*", "javadoc"] if build.with? "docs"
 
     bin.install_symlink "#{libexec}/bin/NetworkServerControl" => "NetworkServerControl"
     bin.install_symlink "#{libexec}/bin/dblook" => "dblook"
@@ -27,5 +28,4 @@ class Derby < Formula
     bin.install_symlink "#{libexec}/bin/stopNetworkServer" => "stopNetworkServer"
     bin.install_symlink "#{libexec}/bin/sysinfo" => "sysinfo"
   end
-
 end
